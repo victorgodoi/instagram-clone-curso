05c362983e51fbe09c7de882d2f42263fdd6e332
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2025-10-02 01:06:18 -0300

    Criando lógica de logar usuário integrada com firebase

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 
   return (
     <div className="App">
-      <Header user={user} />
+      <Header user={user} setUser={setUser} />
     </div>
   );
 }
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,27 @@
 import { useState } from 'react';
 import './header.css';
 import RegisterModal from './components/RegisterModal';
+import { auth } from '../../firebase';
+import { signInWithEmailAndPassword } from 'firebase/auth';
 
-const Header = ({ user }) => {
+const Header = ({ user, setUser }) => {
   const [openModal, setOpenModal] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const logar = (e) => {
+    e.preventDefault();
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setUser(user.displayName || user.email);
+        alert('Logado com sucesso!');
+      })
+      .catch((error) => {
+        alert(error.message || 'Erro ao logar na conta');
+      });
+  };
 
   return(
     <header>
@@ -22,10 +40,24 @@ const Header = ({ user }) => {
           :
             <>
               <div className='login'>
-                <form>
-                  <input type='text' placeholder='Login...' name='userName' />
-                  <input type='password' placeholder='senha...' name='password' />
-                  <input type='submit' value='Logar' name='action' />
+                <form onSubmit={(e) => logar(e)}>
+                  <input
+                    type='text'
+                    placeholder='Login...'
+                    name='email'
+                    onChange={(e) => setEmail(e.target.value)}
+                   />
+                  <input
+                    type='password'
+                    placeholder='senha...'
+                    name='password'
+                    onChange={(e) => setPassword(e.target.value)}
+                  />
+                  <input
+                    type='submit'
+                    value='Logar'
+                    name='action'
+                  />
                 </form>
                 <div className='register'>
                   <button
