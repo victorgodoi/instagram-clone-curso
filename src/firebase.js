13b989659a54a048653279811869dba29f45f018
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2025-10-02 00:47:55 -0300

    Atualizando arquivo de configuração do firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,11 @@
-import firebase from 'firebase';
+// src/firebase.js
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from 'firebase/auth';
+import { getStorage } from 'firebase/storage';
+import { getFunctions } from 'firebase/functions';
 
-const firebaseApp = firebase.initializeApp({
+const firebaseConfig = {
   apiKey: "AIzaSyBUjF_NNCynQXvBjjQjgbHgPPNIK0w0sBA",
   authDomain: "instagram-clone-curso-2a24f.firebaseapp.com",
   projectId: "instagram-clone-curso-2a24f",
@@ -8,11 +13,13 @@ const firebaseApp = firebase.initializeApp({
   messagingSenderId: "341842585913",
   appId: "1:341842585913:web:95c86fd64e0676ff3920e8",
   measurementId: "G-RP1M31L37R"
-});
+};
 
-const db = firebase.firestore();
-const auth = firebase.auth();
-const storage = firebase.storage();
-const functions = firebase.functions();
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+const functions = getFunctions(app);
 
 export { db, auth, storage, functions };
