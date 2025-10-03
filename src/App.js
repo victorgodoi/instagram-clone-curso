6afd3751d96f8b1226601197354ee8eca55e77b2
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2025-10-03 18:21:04 -0300

    Criando a requisição para pegar os dados

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { db } from './firebase';
+import { collection, query, orderBy, onSnapshot } from "firebase/firestore";
 import Header from './components/Header';
+import Hero from './components/Hero';
 
 function App() {
-
   const [user, setUser] = useState(null);
+  const [posts, setPosts] = useState([]);
+  console.log(posts);
+
+  useEffect(() => {
+    const q = query(collection(db, "posts"), orderBy("timestamp", "desc"));
+    const unsubscribe = onSnapshot(q, (snapshot) => {
+      setPosts(snapshot.docs.map((doc) => ({
+        id: doc.id,
+        info: doc.data(),
+      })));
+    });
+
+    return () => unsubscribe();
+  }, []);
 
   return (
     <div className="App">
       <Header user={user} setUser={setUser} />
+      <Hero posts={posts} />
     </div>
   );
 }
