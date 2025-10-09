8e8dd13a260be34dcc63e61c4325330ddbc5c33a
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2025-10-08 23:51:26 -0300

    Adicionando o .env

--- src/firebase.js
+++ src/firebase.js
@@ -6,13 +6,13 @@ import { getStorage } from 'firebase/storage';
 import { getFunctions } from 'firebase/functions';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBUjF_NNCynQXvBjjQjgbHgPPNIK0w0sBA",
-  authDomain: "instagram-clone-curso-2a24f.firebaseapp.com",
-  projectId: "instagram-clone-curso-2a24f",
-  storageBucket: "instagram-clone-curso-2a24f.firebasestorage.app",
-  messagingSenderId: "341842585913",
-  appId: "1:341842585913:web:95c86fd64e0676ff3920e8",
-  measurementId: "G-RP1M31L37R"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
