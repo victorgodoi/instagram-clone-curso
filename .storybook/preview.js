3ae3835e17c9d55e715b6dbfd459c31d0e0c508b
Victor de Godoi Coelho <victordegodoi.c@gmail.com>
2025-10-22 23:05:27 -0300

    Adicionando estilo no preview do storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,10 +1,13 @@
 /** @type { import('@storybook/react-webpack5').Preview } */
+import '../src/App.css';
+import '../src/index.css';
+
 const preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
